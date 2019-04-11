50e906c6f72dd69ceb502171135a0c500f0e7e3b
ＴＲ <public@technotter.com>
2019-04-11 14:03:09 +0900

    基礎処理を実装

--- TR.SimpSer/Main.cs
+++ TR.SimpSer/Main.cs
@@ -4,22 +4,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
 
 namespace TR.SimpSer
 {
   /// <summary>メインの機能をここに実装する。</summary>
   static internal class Main
   {
+    static private SerialPort SP = null;
+    static private ushort DataNum = 0;
+    static private int PortNum = 0;
+    static private int BaudRateNum = 0;
+
+    const int SetVehicleSpecMin = 1;
+    const int SetVehicleSpecMax = 24;
+    const int InitializeMin = 25;
+    const int InitializeMax = 25;
+    const int SetPowerMin = 51;
+    const int SetPowerMax = 51;
+    const int SetBrakeMin = 61;
+    const int SetBrakeMax = 61;
+    const int SetReverserMin = 71;
+    const int SetReverserMax = 71;
+    const int DoorOCMin = 81;
+    const int DoorOCMax = 81;
+    const int SetSignalMin = 91;
+    const int SetSignalMax = 91;
+    const int SetBeaconMin = 100;
+    const int SetBeaconMax = 355;
+    const int KeyDUMin = 500;
+    const int KeyDUMax = 520;
+    const int ElapseMin = 901;
+    const int ElapseMax = 2155;
+
+
     static internal void Load()
     {
 #if DEBUG
       MessageBox.Show("PITempCS Debug Build");//If you don't need, please remove it.
 #endif
+      //ファイル名読む
+      string FileNameStr = typeof(Ats).Assembly.FullName;
+      string[] FileNameStrArr = FileNameStr.Split('.');
+      if (FileNameStrArr[0] != "TR" || FileNameStrArr[1] != "SimpSer")//書式エラー
+      {
+        MessageBox.Show("ERR01 : Configuration format is incorrect.\nThe file name is : " + FileNameStr, "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+      try
+      {
+        PortNum= int.Parse(FileNameStrArr[3]);
+        BaudRateNum = int.Parse(FileNameStrArr[4]);
+      }catch(Exception e)//Int変換エラー
+      {
+        MessageBox.Show("ERR02 : Int.Parse Error\n\nError message\n" + e.Message, "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+      if (PortNum < 0 || PortNum > 255)//ポート番号の範囲
+      {
+        MessageBox.Show("ERR03 : PortNum Range Error\nPlease set in 0 ~ 255", "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+      if (BaudRateNum <= 0)//BaudRateの範囲
+      {
+        MessageBox.Show("ERR04 : BaudRate Range Error\nPlease set in Natural Number", "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+
+      //DataName判別
+      try
+      {
+        DataNum = ushort.Parse(FileNameStrArr[2]);
+      }
+      catch (FormatException)
+      {
+        DataNum = DataNumSearch(in FileNameStrArr[2]);
+        if (DataNum == 0)
+        {
+          MessageBox.Show("ERR09 : This name is not defined.\nName : " + FileNameStrArr[2], "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+          return;
+        }
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show("ERR08 : Int.Parse Error in DataName/Num\n\nError message\n" + e.Message, "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+
+      //シリアル開く
+      try
+      {
+        SP = new SerialPort()
+        {
+          BaudRate = BaudRateNum,
+          PortName = "COM" + PortNum.ToString()
+        };
+      }catch(Exception e)//SerialPort開けなかった
+      {
+        MessageBox.Show("ERR05 : SerialPort Opening Error\n\nError message\n" + e.Message, "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+      if (SP == null) { MessageBox.Show("ERR06 : SerialPort Opening Error\nValue is NULL.", "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+      if (!SP.IsOpen) { MessageBox.Show("ERR07 : SerialPort is Closed.", "SimpSer", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+    }
+
+    static private ushort DataNumSearch(in string s)
+    {
+      switch (s)
+      {
+        case "STTa":return 901;
+        case "SPDf":return 903;
+        default:return 0;
+      }
     }
 
     static internal void Dispose()
     {
-
+      if (SP.IsOpen) SP.Close();
     }
 
     static internal void GetVehicleSpec(Spec s)
@@ -34,7 +137,25 @@ namespace TR.SimpSer
     //Attention! This Method uses "UNSAFE".
     static unsafe internal void Elapse(State st, int* Pa, int* Sa)
     {
-      //If you want to change the Handle state, please access to Ats.Handle
+      if (SP.IsOpen && DataNum >= ElapseMin && DataNum <= ElapseMax)
+      {
+        switch (DataNum)
+        {
+          case 901:
+            //Refer : http://note.websmil.com/csharp/c-%E6%A7%8B%E9%80%A0%E4%BD%93%E3%81%A8%E3%83%90%E3%82%A4%E3%83%88%E9%85%8D%E5%88%97%EF%BC%88byte%EF%BC%89%E3%81%AE%E5%A4%89%E6%8F%9B
+            int sz = Marshal.SizeOf(st);
+            byte[] ba = new byte[sz];
+            fixed(byte* pb = ba)
+            {
+              *(State*)pb = st;
+            }
+            SP?.Write(ba,0,sz);
+            break;
+          case 903:
+            SP?.WriteLine(st.V.ToString());
+            break;
+        }
+      }
     }
 
     static internal void SetPower(int p)
---
+++ TR.SimpSer/readme.md
@@ -0,0 +1,40 @@
+# 実装の解説
+## 概説
+今回はファイル名から設定を読み出し、あらかじめ設定をすることで、楽な実装を実現しています。  
+まぁ本当はDataNameではなくDataNumでの設定をさせたかったのですが、あまりにも不親切だと思ったため、名前での設定もできるようにしています。  
+数字で設定した方がロード時間は短くなると思います。たぶん。
+
+実装について、System.IO.PortsのSerialPortクラスを用いてシリアル通信を行っています。自分で設定しているのはBaudRateとPortNameのみで、それ以外はデフォルトの設定を使用しています。  
+はじめに設定値の有効性やNULLチェックなどを行った後に、DataNameおよびDataNumの検証に入ります。  
+DataNumの検証はint.Parseを用いて行い、エラーを吐きやがったらそれをcatchしてDataNameの検証に入ります。  
+DataNameの検証ではswitch-caseを用いてstringの比較を行い、一致した場合は対応するDataNumを設定します。DataNameがcaseに見当たらなかった場合はエラーを吐きます。  
+DataNumが設定されると、それが0でない限り、シリアル通信が開始されます。DataNumで無効な値が設定された場合も通信が開始されるため、注意してください。  
+ほんとはイベントドリブンにした方が速いんでしょうけど、今回は地道にifとswitch-caseを用いて送信のTFを判断しています。  
+PI終了時は自動でポートが閉じられます。強制終了したときの挙動は知りません。
+
+一応DataNumの値の範囲によって実行場所を特定できるようにしています。  
+DataNumの値の範囲と実行場所の対応は以下の表を参考にしてください。
+|from|to|Method|Remarks|
+|----|----|----|----|
+|0|0|(None)|処理は実行されません。|
+|1|24|SetVehicleSpec|Spec情報群|
+|25|25|Initialize|起動時ブレーキ位置情報|
+|51|51|SetPower|力行ハンドル位置|
+|61|61|SetBrake|制動ハンドル位置|
+|71|71|SetReverser|レバーサーハンドル位置|
+|81|81|DoorOpen/Close|ドア情報|
+|91|91|SetSignal|信号現示番号|
+|100|355|SetBeacon|Beacon情報(StrArrのみ提供)|
+|500|520|KeyDown/Up|キー押下情報|
+|901|999|Elapse|State情報群|
+|1000|1255|Elapse|Panel状態|
+|2000|2155|Elapse|Sound状態|
+
+## DataNumについて
+DataNumは、DataNameと対応した数字です。以下に対応表を置いておきます。
+|Num|Name|Remarks|
+|----|----|----|
+|0|(string.Empty)|シリアル通信は行われません。  
+但し、他の変数の有効性チェックは行われます。|
+|901|STTa|stringのカンマ区切りarray|
+|903|SPDf|現在速度(float)|
